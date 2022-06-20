28c561e2ccee1128e50597f16325f54977325903
bhavesh <bhavesh65321@gmail.com>
2022-06-20 17:54:21 +0530

    sell_buy_upadted

---
+++ Array/sell_and_buy_stock.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max1=0
+        temp1=prices[0]
+        for i in prices:
+            if(i>=temp1):
+                if((i-temp1)>max1):
+                    max1=i-temp1
+            else:
+                temp1=i
+        return max1
+        
